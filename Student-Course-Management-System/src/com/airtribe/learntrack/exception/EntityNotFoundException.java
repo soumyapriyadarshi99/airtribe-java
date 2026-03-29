c8c7fad9bfa01ac33fa7337acde28af7a7c77980
soumyapriyadarshi99 <das.soumyapd@gmail.com>
2026-03-30 02:33:39 +0530

    feat: removed

--- Student-Course-Management-System/src/com/airtribe/learntrack/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.airtribe.learntrack.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
