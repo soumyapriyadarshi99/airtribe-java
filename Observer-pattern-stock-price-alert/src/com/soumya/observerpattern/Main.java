e63cb88e6246d7a311e7c06b247d402fab98c57e
soumyapriyadarshi99 <das.soumyapd@gmail.com>
2026-04-06 00:20:16 +0530

    added initial commit for stock price

---
+++ Observer-pattern-stock-price-alert/src/com/soumya/observerpattern/Main.java
@@ -0,0 +1,20 @@
+/* The Observer Design Pattern is a behavioral design pattern used when one object (subject) needs to notify multiple
+other objects (observers) automatically whenever its state changes.
+
+It defines a one-to-many dependency:
+One Subject (Publisher)
+Many Observers (Subscribers)
+When the subject updates, all observers are notified.
+
+👉 Real-life example:
+Think of a YouTube channel — when a creator uploads a video, all subscribers get notified.
+ */
+
+package com.soumya.observerpattern;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Observer Pattern");
+    }
+}
